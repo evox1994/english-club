99fb173767c4c56a77eaead0ef36244131d0005d
evox1994 <dmitrybatukhtin@gmail.com>
2018-02-14 21:52:58 +0700

    fghjk

---
+++ js/index.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	$('.main-slider').slick({
+		dots: true,
+		speed: 900,
+	});
+
+	$('.levels-button').click(function(){
+		var block = $(this).attr("href");
+		$('.levels-button').parent().removeClass("active");
+		$(this).parent().addClass("active");
+		$('.levels-tabs-item').animate({'opacity': 0}, 300);
+		setTimeout(function(){$('.levels-tabs-item').removeClass("active"); $(block).addClass("active"); $(block).animate({'opacity': 1}, 300);}, 300);
+		return false;
+	});
+
+	$('.reviews-slider').slick({
+		speed: 900,
+		slidesToShow: 2,
+		responsive: [{
+			breakpoint: 1280,
+			settings: {
+				slidesToShow: 1}
+			},
+			{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				dots: true}
+			}
+		]
+	});
+
+	$('.benefits-slider').slick({
+		speed: 900,
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 1280,
+				settings: {
+					slidesToShow: 2
+				},
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+
+	$('.fancybox').fancybox();
+
+	var st = $(this).scrollTop();
+	$(window).scroll(function(){
+		st = $(this).scrollTop();
+		if (st > 0) {
+			$('.header').css({
+				'background-color': '#ffffff',
+				'border-bottom': '1px solid #cccccc',
+			});
+		} else {
+			$('.header').css({
+				'background-color': 'transparent',
+				'border-bottom': '0',
+			});
+		}
+	});
+});
